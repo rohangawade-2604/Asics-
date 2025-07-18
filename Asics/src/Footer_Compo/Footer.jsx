95de7da17201a690b02deef473c407d8e355b931
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-19 01:55:09 +0530

    code

--- Asics/src/Footer_Compo/Footer.jsx
+++ Asics/src/Footer_Compo/Footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import './Footer.css'
 
 export const Footer = () => {
     return (
         <>
-            <div>
+        <div className='footer'>
 
-                <div>
-                    <ul>
+            <div className="footer-container">
+                <div >
+                    <ul className='footer-logo'>
                         <h4>Corporate Info</h4>
                         <li>About ASICS</li>
                         <li>Terms and Conditions</li>
@@ -18,8 +20,8 @@ export const Footer = () => {
                     </ul>
                 </div>
 
-                <div>
-                    <ul>
+                <div >
+                    <ul className='footer-logo'>
                         <h4>Customer Service</h4>
                         <li>Contact Us</li>
                         <li>Return Policies</li>
@@ -30,8 +32,8 @@ export const Footer = () => {
                     </ul>
                 </div>
 
-                <div>
-                    <ul>
+                <div className='footer-logo'>
+                    <ul className='footer-logo'>
                         <h4>Sizing Guides and Resources</h4>
                         <li>Shoe Size and Guides</li>
                         <li>Shoes Width Guides</li>
@@ -41,8 +43,8 @@ export const Footer = () => {
                     </ul>
                 </div>
 
-                <div>
-                    <ul>
+                <div >
+                    <ul className='footer-logo'>
                         <h4>Sign Up to Our NewsLetter</h4>
                         <li>Be the first to hear about new products, exclusive events and online offers.</li>
                         <li>Sign up and Get 10% off on your first Order. T&C applied.</li>
@@ -50,13 +52,31 @@ export const Footer = () => {
 
                         <div>
                             <h4 className='mt-10'>Let's Talk</h4>
-                            <i class="fa-solid fa-phone "></i>
-                            <a href="">0124-6289100</a>
+                            <i className="fa-solid fa-phone "></i>
+                            <a href="" className='phone'>0124-6289100</a>
 
-                            <i class="fa-solid fa-envelope"></i>
-                             <a href=''> customer-service-india@asics.com</a>
+                            <i className="fa-solid fa-envelope"></i>
+                            <a href='' className='email'> customer-service-india@asics.com</a>
                         </div>
+
+                        <h4>Connect With Us</h4>
+
+                        <div className='social-icons'>
+                            <i className="fa-brands fa-facebook"></i>
+                            <i className="fa-brands fa-instagram"></i>
+                            <i className="fa-brands fa-twitter"></i>
+                        </div>
+
+
                     </ul>
+                </div>
+
+            </div>
+
+
+                <div className='footer-bottom'>
+                    <h5>© 2025 ASICS India Pvt. Ltd. All Rights Reserved.</h5>
+                    <h5>The stripe design featured on the sides of the ASICS® shoes is a registered trademark of ASICS Corporation.</h5>
                 </div>
             </div>
         </>
