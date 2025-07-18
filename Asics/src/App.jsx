1a58444989b8f41e45f27175e014dc6ce9abf2dc
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-19 00:53:32 +0530

    code

--- Asics/src/App.jsx
+++ Asics/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
+import { Footer } from './Footer_Compo/Footer'
 
 function App() {
   
 
   return (
     <>
-     
+     <Footer/>
     </>
   )
 }
---
+++ Asics/src/Footer_Compo/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <>
+      <div>
+
+        <div>
+            <ul>
+                <li>About ASICS</li>
+                <li>Terms and Conditions</li>
+                <li>Disclaimer</li>
+                <li>Privacy Policies</li>
+                <li>Cookies Policies</li>
+                <li>Security</li>
+                <li>Sustainabiltiy</li>
+            </ul>
+        </div>
+
+        
+      </div>
+    </>
+  )
+}
+
+
