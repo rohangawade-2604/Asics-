936efa44fb1dc355e81eafbae35596a2594ea752
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-20 12:58:04 +0530

    code

--- Asics/src/Footer_Compo/Footer.jsx
+++ Asics/src/Footer_Compo/Footer.jsx
@@ -8,10 +8,10 @@ export const Footer = () => {
       <div>
         <div className="footer">
 
-          <div className="footer_logo flex ">
+          <div className="footer_logo flex justify-around">
 
             <div className="footer_link">
-              <h1>Corporate Info</h1>
+              <h1 className='font-semibold'>Corporate Info</h1>
               <ul>
                 <li>About ASICS </li>
                 <li>Terms and Conditions</li>
@@ -24,7 +24,7 @@ export const Footer = () => {
             </div>
 
             <div className="footer_link">
-              <h1>Customer Services</h1>
+              <h1 className='font-semibold'>Customer Services</h1>
               <ul>
                 <li>Contact US</li>
                 <li>Return Policy</li>
@@ -36,7 +36,7 @@ export const Footer = () => {
             </div>
 
             <div className="footer_link">
-              <h1>Sizing Guides & Resources</h1>
+              <h1 className='font-semibold'>Sizing Guides & Resources</h1>
               <ul>
                 <li>Shoe size Guide</li>
                 <li>Shoe width Guide</li>
@@ -47,20 +47,31 @@ export const Footer = () => {
             </div>
 
             <div className="footer_link ">
-              <h1>Sign Up to our Newsletter</h1>
+              <h1 className='font-semibold'>Sign Up to our Newsletter</h1>
               <ul>
                 <li>Be the first to hear about new products, exclusive events and online offers.</li>
                 <li>Sign up and Get 10% off on your first Order. T&C applied.</li>
                 <input type="text" name='email' placeholder='you@email.com' />
                 <button>Sign Up</button>
                 <h1>Lets Talk</h1>
-                <div className="contact">
-                  <i className="fa-solid fa-phone "></i>
-                  <a href="" className='mr-5'>0124-6289100</a> 
+
+                <div className="contact ml-10 flex justify-baseline gap-3 ">
+                  <i className="fa-solid fa-phone"></i>
+                  <p className='text-[16px]'>0124-6289100</p>
 
                   <i className="fa-solid fa-envelope"> </i>
-                  <a href="">customer-service-india@asics.com</a> 
+                  <p className='text-[16px]'>customer-service-india@asics.com</p>
                 </div>
+
+                <h1>Connect with Us</h1>
+
+                <div className="social_media flex justify-baseline gap-5">
+                  <i className="fa-brands fa-facebook"></i>
+                  <i className="fa-brands fa-instagram"></i>
+                  <i className="fa-brands fa-twitter"></i>
+                </div>
+
+
 
               </ul>
             </div>
