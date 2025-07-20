ef22a92693911485f4151b56d523ff5bc644bb69
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-20 21:44:23 +0530

    code

--- Asics/src/App.jsx
+++ Asics/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import { Router, Route } from 'react-router-dom'
 import { Footer } from './Footer_Compo/Footer'
+import { Navbar } from './Navbar/Navbar'
 
 
 function App() {
@@ -9,10 +10,11 @@ function App() {
 
   return (
     <>
+    <Navbar/>
     <Router>
 
     </Router>
-      
+
     <Footer/>
     </>
   )
---
+++ Asics/src/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import Asics from './assets/Asics.png'
+import { Link } from 'react-router-dom'
+
+export const Navbar = () => {
+  return (
+    <>
+      <header>
+
+        <div className="logo">
+            <img src={Asics} alt="" />
+        </div>
+
+        <ul>
+            <li>
+                <Link to='/'>Men</Link>
+            </li>
+
+            <li>
+                <Link to='/'>Women</Link>
+            </li>
+
+            <li>
+                <Link to='/'>Kids</Link>
+            </li>
+
+            <li>
+                <Link to='/'>SportStyle</Link>
+            </li>
+
+            <li>
+                <Link to='/'>Collections</Link>
+            </li>
+
+            <li>
+                <Link to='/'>Clothing</Link>
+            </li>
+
+            <li>
+                <Link to='/'>SALE</Link>
+            </li>
+
+            <li>
+                <Link to='/'>Running Club</Link>
+            </li>
+
+            <li>
+                <Link to='/'>Bulk Enquiry</Link>
+            </li>
+        </ul>
+
+      </header>
+    </>
+  )
+}
+
+
