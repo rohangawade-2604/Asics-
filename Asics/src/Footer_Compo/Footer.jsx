bfba707fb5621539924a3ef672b7976223983e19
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-20 11:54:22 +0530

    cod

---
+++ Asics/src/Footer_Compo/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export const Footer = () => {
+    
+  return (
+    <>
+
+      <div>
+        <div className="footer">
+
+          <div className="footer_logo">
+            
+            <div className="footer_link">
+              <h1>Corporate Info</h1>
+              <ul>
+                
+                <li>About ASICS </li>
+                <li>Terms and Conditions</li>
+                <li>Disclaimers</li>
+                <li>Privacy Policy</li>
+                <li>Cookie Policy</li>
+                <li>Security</li>
+                <li>Sustaniability</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+       
+      
+    </>
+  )
+}
+
+
