43ff06ea8f722319e7611242f7007a25586ed29e
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-21 12:11:03 +0530

    code

--- Asics/src/Asics_Home/Home.jsx
+++ Asics/src/Asics_Home/Home.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
-import {Shoes_Slider} from './Shoes_Slider'
+import { Shoes_Slider } from './Shoes_Slider'
 
 export const Home = () => {
     return (
         <>
-        <div className="iframe">
-            <iframe
-                loading="lazy"
-                title="Gumlet video player"
-                src="https://play.gumlet.io/embed/687648c0e448cc1c325d4daa?background=true&autoplay=true&loop=true&disableControls=false"
-                style={{
-                    border: 'none',
-                    height: '100vh',
-                    width: '100%',
-                    overflow: 'hidden',
-                    
-                }}
-                allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture; fullscreen"
-            />
+            <div className="home_container">
+                
+                <div className="iframe">
+                     <iframe
+                    loading="lazy"
+                    title="Gumlet video player"
+                    src="https://play.gumlet.io/embed/687648c0e448cc1c325d4daa?background=true&autoplay=true&loop=true&disableControls=false"
+                    style={{
+                        border: 'none',
+                        height: '100vh',
+                        width: '100%',
+                        overflow: 'hidden',
 
-            <Shoes_Slider/>
-        </div>
-            
+                    }}
+                    allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture; fullscreen"
+                />
+                </div>
+               
+
+                <div className='shoes_slider'>
+                    <Shoes_Slider />
+                </div>
+
+            </div>
+
         </>
     )
 }
--- Asics/src/Asics_Home/Shoes_Slider.jsx
+++ Asics/src/Asics_Home/Shoes_Slider.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Shoes_Slider = () => {
   return (
     <>
-      
+      <h1>hellow rohan</h1>
     </>
   )
 }
