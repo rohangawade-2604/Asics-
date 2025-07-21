c8727d2378ebe80e4c2ebf32b53f9d8e4f181c4b
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-21 12:46:34 +0530

    code

--- Asics/src/Asics_Home/Shoes_Slider.jsx
+++ Asics/src/Asics_Home/Shoes_Slider.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Homepic from '../assets/Homepic.png'
+import Homepic2 from '../assets/Homepic2.png'
+import Homepic3 from '../assets/Homepic3.png'
+import './Shoes_Slider.css'
+import HomePic4 from '../assets/Homepic4.png'
 
 export const Shoes_Slider = () => {
   return (
     <>
-      <h1>hellow rohan</h1>
+    <h1 className='text-[36px] text-center'>Limited Series Collection</h1>
+      <div className="slider">
+        <figure className='figure'>
+            <img src={Homepic} alt="" />
+            <img src={Homepic2} alt="" />
+            <img src={Homepic3} alt="" />
+        
+        </figure>
+      </div>
     </>
   )
 }
