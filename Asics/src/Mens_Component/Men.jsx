89df4d85fc14edfc192e3ab92f718f3a21a3adf3
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-24 01:51:21 +0530

    code

--- Asics/src/Mens_Component/Men.jsx
+++ Asics/src/Mens_Component/Men.jsx
@@ -6,7 +6,8 @@ import Running from '../assets/Running.png'
 import Tennis from '../assets/Tennis.png'
 import Indoors from '../assets/Indoor.png'
 import Cricket from '../assets/Cricket.png'
-import Homepic2 from '../assets/Homepic.png'
+import Homepic2 from '../assets/Homepic2.png'
+import MensGel from '../assets/MensGel.png'
 export const Men = () => {
   return (
     <>
@@ -16,16 +17,16 @@ export const Men = () => {
 
           {/*---------------- image_Slider section------------------ */}
 
-            <div className="image1">
-              <figure>
-                <img src={men} alt="" />
-                <img src={men1} alt="" />
-                <img src={men} alt="" />
-                <img src={men1} alt="" />
-                <img src={men} alt="" />
-              </figure>
+          <div className="image1">
+            <figure>
+              <img src={men} alt="" />
+              <img src={men1} alt="" />
+              <img src={men} alt="" />
+              <img src={men1} alt="" />
+              <img src={men} alt="" />
+            </figure>
 
-            </div>
+          </div>
 
 
           {/*---------------- Mens_Collections section------------------ */}
@@ -36,49 +37,49 @@ export const Men = () => {
 
             <div className="buttons flex flex-row justify-around mx-10 my-7 mb-10 ">
 
-                  <button type='button' className='border-2 gap-1 w-45 h-13  rounded-[25px] bg-[#001f62] text-white transition delay-100 ease-in-out hover:bg-amber-800 cursor-pointer'>Footwear</button>
+              <button type='button' className='border-2 gap-1 w-45 h-13  rounded-[25px] bg-[#001f62] text-white transition delay-100 ease-in-out hover:bg-amber-800 cursor-pointer'>Footwear</button>
 
-                  <button type='button' className='border-2 gap-1 w-45 h-13  rounded-[25px] bg-[#001f62] text-white transition delay-100 ease-in-out hover:bg-amber-800 cursor-pointer'>Clothing</button>
+              <button type='button' className='border-2 gap-1 w-45 h-13  rounded-[25px] bg-[#001f62] text-white transition delay-100 ease-in-out hover:bg-amber-800 cursor-pointer'>Clothing</button>
 
-                  <button type='button' className='border-2 gap-1 w-45 h-13  rounded-[25px] bg-[#001f62] text-white transition delay-100 ease-in-out hover:bg-amber-800 cursor-pointer'>Accessiories</button>
+              <button type='button' className='border-2 gap-1 w-45 h-13  rounded-[25px] bg-[#001f62] text-white transition delay-100 ease-in-out hover:bg-amber-800 cursor-pointer'>Accessiories</button>
 
-                  <button type='button' className='border-2 gap-1 w-45 h-13  rounded-[25px] bg-[#001f62] text-white transition delay-100 ease-in-out hover:bg-amber-800 cursor-pointer'>SportStyle</button>
+              <button type='button' className='border-2 gap-1 w-45 h-13  rounded-[25px] bg-[#001f62] text-white transition delay-100 ease-in-out hover:bg-amber-800 cursor-pointer'>SportStyle</button>
 
-                  <button type='button' className='border-2 gap-1 w-45 h-13  rounded-[25px] bg-[#001f62] text-white transition delay-100 ease-in-out hover:bg-amber-800 cursor-pointer'>Online Exclusive</button>
+              <button type='button' className='border-2 gap-1 w-45 h-13  rounded-[25px] bg-[#001f62] text-white transition delay-100 ease-in-out hover:bg-amber-800 cursor-pointer'>Online Exclusive</button>
 
-                  <button type='button' className='border-2 gap-1 w-45 h-13  rounded-[25px] bg-[#001f62] text-white transition delay-100 ease-in-out hover:bg-amber-800 cursor-pointer'>Sale Oulet</button>
+              <button type='button' className='border-2 gap-1 w-45 h-13  rounded-[25px] bg-[#001f62] text-white transition delay-100 ease-in-out hover:bg-amber-800 cursor-pointer'>Sale Oulet</button>
 
             </div>
-              <hr className='mx-20' />
+            <hr className='mx-20' />
           </div>
 
-        {/*-------------------- Shop by Sports Sections----------------------- */}
+          {/*-------------------- Shop by Sports Sections----------------------- */}
 
           <div className="Sports">
 
-            <h1 className='text-[36px] text-center text-[#312955] my-5'>Shop by Sports</h1> 
+            <h1 className='text-[36px] text-center text-[#312955] my-5'>Shop by Sports</h1>
 
             <div className="cards_Sections flex flex-row justify-around mx-20 my-7 mb-10 gap-10 ">
 
-                  <div className="cards ">
-                    <img src={Running} alt="" className='h-60 '/>
-                    <button className='border-2 w-45 h-10 rounded-[25px] my-3 ml-15 cursor-pointer text-white bg-[#001f62]  transition delay-100 ease-in-out hover:bg-amber-800 '>Running</button>
-                  </div>
+              <div className="cards ">
+                <img src={Running} alt="" className='h-60 ' />
+                <button className='border-2 w-45 h-10 rounded-[25px] my-3 ml-15 cursor-pointer text-white bg-[#001f62]  transition delay-100 ease-in-out hover:bg-amber-800 '>Running</button>
+              </div>
 
-                  <div className="cards1">
-                    <img src={Tennis} alt="" className='h-60'/>
-                    <button className='border-2 w-45 h-10 rounded-[25px] my-3 ml-15 cursor-pointer text-white bg-[#001f62]  transition delay-100 ease-in-out hover:bg-amber-800 '>Tennis</button>
-                  </div>
+              <div className="cards1">
+                <img src={Tennis} alt="" className='h-60' />
+                <button className='border-2 w-45 h-10 rounded-[25px] my-3 ml-15 cursor-pointer text-white bg-[#001f62]  transition delay-100 ease-in-out hover:bg-amber-800 '>Tennis</button>
+              </div>
 
-                  <div className="cards2"> 
-                    <img src={Indoors} alt="" className='h-60'/>
-                    <button className='border-2 w-45 h-10 rounded-[25px] my-3 ml-15 cursor-pointer text-white bg-[#001f62]  transition delay-100 ease-in-out hover:bg-amber-800 '>Indoors</button>
-                  </div>
+              <div className="cards2">
+                <img src={Indoors} alt="" className='h-60' />
+                <button className='border-2 w-45 h-10 rounded-[25px] my-3 ml-15 cursor-pointer text-white bg-[#001f62]  transition delay-100 ease-in-out hover:bg-amber-800 '>Indoors</button>
+              </div>
 
-                  <div className="cards3">
-                    <img src={Cricket} alt="" className='h-60'/>
-                    <button className='border-2 w-45 h-10 rounded-[25px] my-3 ml-15 cursor-pointer text-white bg-[#001f62]  transition delay-100 ease-in-out hover:bg-amber-800 '>Cricket</button>
-                  </div>
+              <div className="cards3">
+                <img src={Cricket} alt="" className='h-60' />
+                <button className='border-2 w-45 h-10 rounded-[25px] my-3 ml-15 cursor-pointer text-white bg-[#001f62]  transition delay-100 ease-in-out hover:bg-amber-800 '>Cricket</button>
+              </div>
             </div>
           </div>
 
@@ -89,7 +90,15 @@ export const Men = () => {
             <h1 className='text-[36px] text-center text-[#312955] my-5'>Luxe Pack</h1>
 
             <img src={Homepic2} alt="" />
-            </div>
+          </div>
+
+          {/* ------------- GEL-KAYANO Section ------------------ */}
+
+          <div className="gelkayano">
+            <h1 className='text-[36px] text-center text-[#312955]'>GEL-KAYANO™ 32</h1>
+            <img src={MensGel} alt="" className='cursor-pointer' />
+          </div>
+
 
 
         </div>
