0caa7d75eccf4fc44ab23b90958609f83c6ed15c
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-27 00:14:56 +0530

    code

--- Asics/src/RunningClub/Running_club.jsx
+++ Asics/src/RunningClub/Running_club.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import './running.css'
+import Runningclub from '../assets/Runningclub.png'
 
 export const Running_club = () => {
   return (
     <>
 
-      <button>Squeeze me</button>
+    <div className="Running_club">
+      <div className="running_container ">
+
+        <div className="img1 h-[600px] w-full bg-cover bg-center relative " style={{ backgroundImage: `url(${Runningclub})` }}> 
+          <figure className='absolute bottom-20 left-50 '>
+           <h1 className='text-4xl font-bold text-white'>ASICS RUNNING CLUB</h1>
+           <p className='text-white w-[60%] my-5'>Come join us for our upcoming sessions! Take a look at our monthly schedule for the upcoming training programmes and sign up for the sessions with our highly-experience coaches.</p>
+           <button className='effect_button mr-10 text-[16px]'>Sign Up here</button>
+            <button className='effect_button '>Join Facebook Group</button>           
+          </figure>
+        </div>
+
+
+      </div>
+    </div>
+      
 
     </>
   )
