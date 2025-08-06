36c2cdb4f8d38b46c06f2ab150dd1b4c82d3e763
rohangawade-2604 <rohangawade2604@gmail.com>
2025-08-07 01:16:32 +0530

    code for thd day

--- Asics/src/Home/Bulk_Enquiry.jsx
+++ Asics/src/Home/Bulk_Enquiry.jsx
@@ -14,28 +14,48 @@ export const Bulk_Enquiry = () => {
             </div>
 
 
-        <div className="enquiry_info flex justify-around items-center my-8">
-            <div className="special_pricing flex gap-4 ">
-                <img src={Rupees} alt="" />
+            <div className="enquiry_info flex justify-around items-center my-8">
+                <div className="special_pricing flex gap-4 ">
+                    <img src={Rupees} alt="" />
 
-                <div className="containt">
-                    <h1>Special Pricing</h1>
-                    <p className=''>Unlock Special prices with Bulk Orders</p>
+                    <div className="containt">
+                        <h1>Special Pricing</h1>
+                        <p className=''>Unlock Special prices with Bulk Orders</p>
+                    </div>
+                </div>
+
+                <div className="GST_Benefits flex gap-4">
+                <i className="fa-regular fa-file text-[50px] text-[#001f62]"></i>
+
+                    <div className="containt">
+                        <h1>GST Benefits</h1>
+                        <p className=''>Save up to 30% on your purchases by claiming the GST inputs tax credits</p>
+                    </div>
                 </div>
             </div>
 
-            <div className="GST_Benefits flex gap-4">
-               <i className="fa-regular fa-file text-[50px] text-[#001f62]"></i>
 
-                <div className="containt">
-                    <h1>GST Benefits</h1>
-                    <p className=''>Save up to 30% on your purchases by claiming the GST inputs tax credits</p>
+            <form action="">
+                <h1 className='text-[36px] text-[#312955] text-center'>Enquiry Form</h1>
+                <div className="name_organisation flex flex-row gap-4 ">
+                    <input type="text" name="name" id="name" placeholder='Name of the Representative' className='border-2 p-3 w-100 mt-5'/>
+                    <input type="text" name="organisation" id="organisation" placeholder='Name of the Organisation' className='border-2 p-3 w-100 mt-5'/>
                 </div>
-            </div>
 
+                <div className="email_contact flex flex-row gap-4 ">
+                    <input type="email" name="email" id="email" placeholder='Enter the Email' className='border-2 p-3 w-100 mt-5'/>
+                    <input type="text" name="contact" id="contact" placeholder='Contact Number' className='border-2 p-3 w-100 mt-5'/>
+                </div>
 
+                <div className="Required_Quantity flex flex-row gap-4 ">
+                    <input type="text" name="requirement" id="requirement" placeholder='Requirement' className='border-2 p-3 w-100 mt-5'/>
+                    <input type="number" name="quantity" id="quantity" placeholder='Required Quantity' className='border-2 p-3 w-100 mt-5'/>
+                </div>
 
-        </div>
+                <div className="msg">
+                    <textarea name="msg" id="" cols="30" rows="10" placeholder='Message'></textarea>
+                </div>
+            </form>
         </div>
     </div>
     </>
