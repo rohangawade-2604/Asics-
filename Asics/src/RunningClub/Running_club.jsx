ddcc211d0a25d24c2e69d13537aca3b766266503
rohangawade-2604 <rohangawade2604@gmail.com>
2025-08-11 14:02:44 +0530

    code

--- Asics/src/RunningClub/Running_club.jsx
+++ Asics/src/RunningClub/Running_club.jsx
@@ -28,21 +28,43 @@ export const Running_club = () => {
 
 
           <div className="club_members">
-              <h1 className='text-[36px] text-center text-[#312955] '>CLUB MEMBERS IN ACTION</h1>
+            <h1 className='text-[36px] text-center text-[#312955] '>CLUB MEMBERS IN ACTION</h1>
 
-              <div className="imgs flex justify-evently gap-5 p-3  pl-[22%]">
-                <img src={CB1} alt="" />
-                <img src={CB2} alt="" />
-                <img src={CB3} alt="" />
-                <img src={CB4} alt="" />
-              </div>
+            <div className="imgs flex justify-evently gap-5 p-3  pl-[22%]">
+                <div className="wrapper">
+                    <img src={CB1} alt="" />
+                </div>
 
-              <div className="img1 flex justify-evently gap-5 p-3  pl-[22%]">
-                <img src={CB5} alt="" />
-                <img src={CB6} alt="" />
-                <img src={CB7} alt="" />
-                <img src={CB8} alt="" />
-              </div>
+                <div className="wrapper">
+                    <img src={CB2} alt="" />
+                </div>
+
+                <div className="wrapper">
+                    <img src={CB3} alt="" />
+                </div>
+
+                <div className="wrapper">
+                    <img src={CB4} alt="" />
+                </div>
+
+            </div>
+
+            <div className="img1 flex justify-evently gap-5 p-3  pl-[22%]">
+
+                <div className="wrapper">
+                   <img src={CB5} alt="" />
+                </div>
+                <div className="wrapper">
+                    <img src={CB6} alt="" />
+                </div>
+                <div className="wrapper">
+                    <img src={CB7} alt="" />
+                </div>
+                <div className="wrapper">
+                     <img src={CB8} alt="" />
+                </div>                    
+  
+            </div>
           </div>
 
 
