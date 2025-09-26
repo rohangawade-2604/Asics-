5db87b32e1a90702de910c071322309e1b6ea7a5
rohangawade-2604 <rohangawade2604@gmail.com>
2025-09-26 16:33:59 +0530

    code

--- Asics/src/Kids_Component/DropDown.jsx
+++ Asics/src/Kids_Component/DropDown.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 
 
-export const DropDown = () => {
+export const DropDown = ({ title, Option, isOpen, isToggle }) => {
   return (
     <>
-      <h1>hellow rworld</h1>
+      
+      <div className="Container inline-block border-1 ml-3 p-2 relative">
+
+        
+              <button className='flex flex-row gap-3' onClick={isToggle}>
+                {title} 
+                <span><i className="fa-solid fa-angle-down"></i></span>
+              </button>
+            
+          {
+            isOpen && (
+              <div className='flex flex-col absolute'>
+                {
+                  Option.map((el, i) => (
+                    <div  className=''> 
+                      <label className=' ' key={i} >
+                        <input type="checkbox "/>
+                        {el}
+                      </label>
+                    </div>
+                  ))
+                }
+              </div>
+
+            )
+          }
+
+
+          </div>
+        
+
     </>
   )
 }
--- Asics/src/Kids_Component/Kids.jsx
+++ Asics/src/Kids_Component/Kids.jsx
@@ -1,12 +1,43 @@
 import React from 'react'
 import { DropDown } from './DropDown'
+import { useState } from 'react'
 
 
 export const Kids = () => {
+
+  const [isOpen, setIsOpen] = useState(null)
+
+  const handleToggle = (title) => {
+    setIsOpen(isOpen === title ? null : title)
+  }
+
   return (
     <>
-      
+      <DropDown
+      title="CATEGORY"
+      Option={['Category1','Category2','Category3']}
+      isOpen={isOpen === "CATEGORY"}
+      isToggle={() => handleToggle("CATEGORY")}
+      /> 
+
+      <DropDown
+      title="BRAND"
+      Option={['Brand1','Brand2','Brand3']}
+      isOpen={isOpen === "BRAND"}
+      isToggle={() => handleToggle("BRAND")}
+      />
+
+      <DropDown
+      title="PRICE"
+      Option={['Price1','Price2','Price3']}
+      isOpen={isOpen === "PRICE"}
+      isToggle={() => handleToggle("PRICE")}
+      />
+
+                                                                                                                                                                                                                                            
     </>
+
+
   )
 }
 
